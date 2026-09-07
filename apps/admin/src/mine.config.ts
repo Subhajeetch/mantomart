cd424316c6eb367014ecbf28f6786ab72a5365a6
Subhajeetch <subhajeetchowdhury954@gmail.com>
2026-09-07 15:15:15 +0000

    chore: changed some images

--- apps/admin/src/mine.config.ts
+++ apps/admin/src/mine.config.ts
@@ -1,5 +1,5 @@
 const config = {
-    logoShort: "/logos/mantomart-logo-short.png", //1:1
+    logoShort: "/logos/mantomart-admin-short.webp", //1:1
     logoLong: "/logos/mantomart-logo.png",
     brandName: "Mantomart",
     storeFrontURI: "https://mantomart.com"
